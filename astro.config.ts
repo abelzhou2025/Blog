c29a57244ffceebf9f029365919c02bf66673186
abelzhou2025 <abelzhou3399@gmail.com>
2026-01-04 09:52:17 +0800

    fix base path case for github pages

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,8 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: 'https://abelzhou2025.github.io',
+  base: '/Blog',
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
